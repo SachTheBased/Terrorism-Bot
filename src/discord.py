f0a153494b0c621da8d2f49189db617fc79658e0
Sachs <96741564+SachTheBased@users.noreply.github.com>
2022-05-14 12:49:36 +0000

    First working version

--- src/discord.py
+++ src/discord.py
@@ -1,9 +1,18 @@
 import aiohttp
+import asyncio
 
 class Context:
   def __init__(self, context, headers):
     self.context = context
     self.headers = headers
+    self.api = "https://discord.com/api/v9"
 
   async def send(self, msg):
-    
+    async with aiohttp.ClientSession() as session:
+      while True:
+        async with session.post(f'{self.api}/channels/{self.context["d"]["channel_id"]}/messages', headers = self.headers, json = {'content': msg}) as message:
+          if message.status == 429:
+            message = await message.json()
+            asyncio.sleep(message['retry_after'])
+          else:
+            break
--- src/main.py
+++ src/main.py
@@ -10,6 +10,13 @@ except:
   import aiohttp
   import asyncio
 
+token = input("Enter token: ")
+headers = {"Authorization": token}
+
+print("""Welcome to Terrorism SB!!!
+
+Prefix -> $""")
+
 async def heartbeat(ws, interval):
   while True:
     await asyncio.sleep(interval / 1000)
@@ -19,6 +26,18 @@ async def main(token):
   async with aiohttp.ClientSession() as session:
     # Yes I use v6, cope harder you discord.py coders
     async with session.ws_connect("wss://gateway.discord.gg?v=6&encoding=json") as ws:
-      await ws.send_json({""})
+      await ws.send_json({"op": 2,"d": {"token": token, "properties": {"$os": "linux","$browser": "my_library","$device": "my_library"}}})
       async for messages in ws:
-        ctx = discord.Context(json.load(messages))
+        data = json.loads(messages.data)
+        ctx = discord.Context(json.loads(messages.data), headers = headers)
+        
+        if data["op"] == 10:
+          asyncio.ensure_future(heartbeat(ws, data['d']['heartbeat_interval']))
+        elif data["op"] == 0:
+          if data['t'] == 'MESSAGE_CREATE':
+            if data['d']['content'] == "help":
+              await ctx.send("```Commands\nNone yet```")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(token))
+loop.close()
